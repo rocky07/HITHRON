3b220102a7f55c9d377a73b89c7d26ffe11612bb
rakeshramesan <rakesh.ramesan@tmhighland.com>
2023-03-25 10:14:17 -0500

    init set up

---
+++ src/main/java/com/bourntec/HITHRON/HithronApplication.java
@@ -0,0 +1,13 @@
+package com.bourntec.HITHRON;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HithronApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HithronApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bourntec/HITHRON/HithronApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bourntec.HITHRON;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HithronApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
